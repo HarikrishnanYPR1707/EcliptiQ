0f8d637a9716473254244f578bf641f9acf6b63a
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-29 11:32:35 +0530

    Added height without navbar for other section than navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      height: {
+        heightWithoutNavbar: "calc(100vh - 80px)",
+      },
     },
   },
   plugins: [],
