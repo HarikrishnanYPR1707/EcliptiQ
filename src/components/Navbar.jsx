3c19518e02e9858847e87c0a687035482c7a9e0a
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-29 11:32:54 +0530

    Done with navbar and added height to home section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex h-[80px] items-center justify-center border p-2">
+      <div className="relative flex h-full w-full max-w-[1550px] items-center  justify-center border border-red-500">
+        <div className="grid h-full w-[80px] place-content-center border border-purple-500 font-black uppercase">
+          {/* <img src="" alt="" /> */}
+          logo
+        </div>
+        <button className="absolute right-6 rounded-xl bg-green-600 px-5 py-2 font-extrabold active:bg-green-700">
+          SignUp
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
