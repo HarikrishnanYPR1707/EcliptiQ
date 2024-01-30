e9e5bf258cb814bc953efe7453390efb6edca3b1
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-30 23:12:34 +0530

    Added Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex h-[80px] items-center justify-center border p-2">
+    <div className="flex h-[80px] items-center justify-center border px-6 py-2">
       <div className="relative flex h-full w-full max-w-[1550px] items-center  justify-center border border-red-500">
         <div className="grid h-full w-[80px] place-content-center border border-purple-500 font-black uppercase">
           {/* <img src="" alt="" /> */}
           logo
         </div>
-        <button className="absolute right-6 rounded-xl bg-green-600 px-5 py-2 font-bold active:bg-green-700">
+        <button className="absolute right-0 rounded-xl bg-green-600 px-5 py-2 font-bold active:bg-green-700">
           SignUp
         </button>
       </div>
