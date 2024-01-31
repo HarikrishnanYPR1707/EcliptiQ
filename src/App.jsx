1b131b22400dec7f32b446f1550b9a90b37de1de
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-01 01:40:21 +0530

    Added template component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Template from "./pages/Template";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <div className="mx-auto h-heightWithoutNavbar w-full max-w-[1550px] border border-purple-500">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/templates" element={<div>Template Page</div>} />
+            <Route path="/templates" element={<Template />} />
             {/* <Route path="/templates/Banking" element={<div>Template Page</div>} /> */}
             {/* <Route path="/templates/Banking/template1" element={<div>Template Page</div>} /> */}
           </Routes>
