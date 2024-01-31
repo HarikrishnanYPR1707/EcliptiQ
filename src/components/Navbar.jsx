0a6b02e0eba90a9c5fdef82f3377dbffe2a279d6
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-31 17:42:41 +0530

    Added Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
 const Navbar = () => {
-  const [data, setdata] = useState();
   return (
     <div className="flex h-[80px] items-center justify-center border px-6 py-2">
       <div className="relative flex h-full w-full max-w-[1550px] items-center  justify-center border border-red-500">
-        <div className="font-honk grid h-full place-content-center border border-purple-500 text-5xl font-black uppercase">
+        <div className="font-honk grid h-full place-content-center text-5xl font-black uppercase">
           {/* <img src="" alt="" /> */}
           EcliptiQ
         </div>
@@ -18,3 +17,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// border border-purple-500
