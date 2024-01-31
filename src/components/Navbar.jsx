b123837f83c3fc99f7aff4f48d8ddce230c22b47
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-31 17:36:52 +0530

    Added Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [data, setdata] = useState();
   return (
     <div className="flex h-[80px] items-center justify-center border px-6 py-2">
       <div className="relative flex h-full w-full max-w-[1550px] items-center  justify-center border border-red-500">
-        <div className="grid h-full w-[80px] place-content-center border border-purple-500 font-black uppercase">
+        <div className="font-honk grid h-full place-content-center border border-purple-500 text-5xl font-black uppercase">
           {/* <img src="" alt="" /> */}
-          logo
+          EcliptiQ
         </div>
         <button className="absolute right-0 rounded-xl bg-green-600 px-5 py-2 font-bold active:bg-green-700">
           SignUp
