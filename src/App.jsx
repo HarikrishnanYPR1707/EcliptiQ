627109d6b0fa017b7c46605b53398fae04b9b7f5
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-01 00:49:23 +0530

    Wrapped all the routes inside a div to limit the container width to 1500px

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ const App = () => {
     <>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/templates" element={<div>Template Page</div>} />
-          {/* <Route path="/templates/Banking" element={<div>Template Page</div>} /> */}
-          {/* <Route path="/templates/Banking/template1" element={<div>Template Page</div>} /> */}
-        </Routes>
+        <div className="mx-auto h-heightWithoutNavbar w-full max-w-[1550px] border border-purple-500">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/templates" element={<div>Template Page</div>} />
+            {/* <Route path="/templates/Banking" element={<div>Template Page</div>} /> */}
+            {/* <Route path="/templates/Banking/template1" element={<div>Template Page</div>} /> */}
+          </Routes>
+        </div>
       </Router>
     </>
   );
