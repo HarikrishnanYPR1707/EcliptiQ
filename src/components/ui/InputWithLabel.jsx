ca671feee2f6a61928c3ec777478ade13c90e003
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-01 10:23:31 +0530

    Added props

--- src/components/ui/InputWithLabel.jsx
+++ src/components/ui/InputWithLabel.jsx
@@ -1,11 +1,11 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function InputWithLabel({ label, type, placeholder }) {
+export function InputWithLabel({ label, type, placeholder, ...props }) {
   return (
-    <div className="grid w-full max-w-sm items-center gap-1.5">
+    <div className="grid w-full max-w-sm items-center gap-1.5 border border-rose-500">
       <Label htmlFor="email">{label}</Label>
-      <Input type={type} id="email" placeholder={placeholder} />
+      <Input type={type} id="email" placeholder={placeholder} {...props} />
     </div>
   );
 }
