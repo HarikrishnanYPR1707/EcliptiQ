044b7443847c612d29545f7b0662de79c1aedc26
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-02 23:21:24 +0530

    Added Changes

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -5,17 +5,17 @@ import { Label } from "@/components/ui/label";
 
 const Information = () => {
   return (
-    <div className="mx-auto flex max-w-[900px] items-start justify-center border border-white py-4">
-      <div className="w-full border border-amber-500">
+    <div className="mx-auto flex max-w-[900px] items-start justify-center py-4">
+      <div className="w-full ">
         <h1 className="text-center text-[22px] font-black uppercase text-[#6d28d9] sm:text-4xl">
           Enter your information
         </h1>
         {/* Basic Information  */}
-        <div className="mt-4 border border-amber-500 ">
-          <h1 className="text-xl font-black text-gray-300 underline">
+        <div className="mt-4  ">
+          <h1 className="text-center text-xl font-black text-gray-300 underline lg:text-3xl">
             Basic Information
           </h1>
-          <div className="mt-4 grid grid-cols-1 justify-items-center gap-4 border border-amber-500 sm:grid-cols-2">
+          <div className="mt-4 grid grid-cols-1 justify-items-center gap-4  sm:grid-cols-2">
             <InputWithLabel
               label="First Name"
               type="text"
@@ -62,11 +62,11 @@ const Information = () => {
           </div>
         </div>
         {/* Education Details */}
-        <div className="mt-4 border border-amber-500 ">
-          <h1 className="text-xl font-black text-gray-300 underline">
+        <div className="mt-4  ">
+          <h1 className="text-center text-xl font-black text-gray-300 underline lg:text-3xl">
             Education Details
           </h1>
-          <div className="mt-4 border border-amber-500">
+          <div className="mt-4 ">
             {/* college section */}
             <div className="mb-4 mt-4 grid w-full grid-cols-1 items-center justify-items-center gap-4 border-b-2 pb-4 sm:grid-cols-2">
               <InputWithLabel
@@ -101,11 +101,116 @@ const Information = () => {
           </div>
         </div>
         {/* Experience Section */}
-        <div className="mt-4 border border-amber-500 ">
-          <h1 className="text-xl font-black text-gray-300 underline">
+        <div className="mt-4  ">
+          <h1 className="text-center text-xl font-black text-gray-300 underline lg:text-3xl">
             Experience Section
           </h1>
-          <div className="mt-4 border border-amber-500"></div>
+          <div className="mt-4 ">
+            <h1 className="mb-4 text-lg text-gray-300 underline">
+              Experience 1
+            </h1>
+            <div className="grid grid-cols-1 justify-items-center gap-4 sm:grid-cols-2">
+              <InputWithLabel
+                label="Institute/Organisation"
+                type="text"
+                placeholder="Enter your Organisation Name..."
+                required
+              />
+              <InputWithLabel
+                label="Position"
+                type="text"
+                placeholder="Enter your Position..."
+                required
+              />
+              <InputWithLabel
+                label="Duration"
+                type="text"
+                placeholder="Enter your duration in that organisation..."
+                required
+              />
+              <InputWithLabel
+                label="Description"
+                type="text"
+                placeholder="Enter the description..."
+                required
+              />
+            </div>
+          </div>
+          <div className="mt-4 ">
+            <h1 className="mb-4 text-lg text-gray-300 underline">
+              Experience 2
+            </h1>
+            <div className="grid grid-cols-1 justify-items-center gap-4 sm:grid-cols-2">
+              <InputWithLabel
+                label="Institute/Organisation"
+                type="text"
+                placeholder="Enter your Organisation Name..."
+                required
+              />
+              <InputWithLabel
+                label="Position"
+                type="text"
+                placeholder="Enter your Position..."
+                required
+              />
+              <InputWithLabel
+                label="Duration"
+                type="text"
+                placeholder="Enter your duration in that organisation..."
+                required
+              />
+              <InputWithLabel
+                label="Description"
+                type="text"
+                placeholder="Enter the description..."
+                required
+              />
+            </div>
+          </div>
+        </div>
+        {/* Skills */}
+        <div className="mt-4  ">
+          <h1 className="text-center text-xl font-black text-gray-300 underline lg:text-3xl">
+            Skills
+          </h1>
+          <div className="mt-4 grid grid-cols-1 justify-items-center gap-4 sm:grid-cols-2">
+            <InputWithLabel
+              label="Skill 1"
+              type="text"
+              placeholder="Enter skill 1..."
+              required
+            />
+            <InputWithLabel
+              label="Skill 2"
+              type="text"
+              placeholder="Enter skill 2..."
+              required
+            />
+            <InputWithLabel
+              label="Skill 3"
+              type="text"
+              placeholder="Enter skill 3..."
+              required
+            />
+            <InputWithLabel
+              label="Skill 4"
+              type="text"
+              placeholder="Enter skill 4..."
+              required
+            />
+            <InputWithLabel
+              label="Skill 5"
+              type="text"
+              placeholder="Enter skill 5..."
+              required
+            />
+            <InputWithLabel
+              label="Skill 6"
+              type="text"
+              placeholder="Enter skill 6..."
+              required
+            />
+          </div>
         </div>
         <div className="mt-4 grid justify-items-center">
           <Button className="px-10 font-black uppercase">Save</Button>
