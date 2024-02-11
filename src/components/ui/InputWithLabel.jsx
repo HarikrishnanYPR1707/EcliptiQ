c47483bc84a7e86c6963d20d4cbea7248c997ba7
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-11 21:35:59 +0530

    added max width for input with label

--- src/components/ui/InputWithLabel.jsx
+++ src/components/ui/InputWithLabel.jsx
@@ -3,7 +3,7 @@ import { Label } from "@/components/ui/label";
 
 export function InputWithLabel({ label, type, placeholder, ...props }) {
   return (
-    <div className="grid w-full max-w-sm items-center gap-1.5">
+    <div className="grid w-full max-w-[340px] items-center gap-1.5">
       <Label htmlFor="email">{label}</Label>
       <Input type={type} id="email" placeholder={placeholder} {...props} />
     </div>
