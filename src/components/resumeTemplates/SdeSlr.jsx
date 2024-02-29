b530548bac17eb3c60c74b97ecb0792db63c8017
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-29 22:03:15 +0530

    Added Changes

--- src/components/resumeTemplates/SdeSlr.jsx
+++ src/components/resumeTemplates/SdeSlr.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 const SdeSlr = () => {
-  return <div>slr</div>;
+  const sdeSlrComponentDownloadRef = useRef(null);
+
+  const handlePrint = useReactToPrint({
+    content: () => sdeSlrComponentDownloadRef.current,
+    documentTitle: "SDE-SLR-Resume",
+    // pageStyle: "print",
+  });
+
+  return (
+    <div className="h-[2000px] border-[3px] border-rose-500">
+      <div
+        ref={sdeSlrComponentDownloadRef}
+        className="aspect-[1/1.4142] w-full max-w-[800px] border-[5px] border-purple-500"
+      >
+        {Array(2)
+          .fill(true)
+          .map(() => (
+            <div className="w-full bg-red-200 py-2 font-bold uppercase text-black">
+              alsdjflkj
+            </div>
+          ))}
+      </div>
+      <button
+        onClick={handlePrint}
+        className="rounded-lg bg-purple-500 px-3 py-2 text-sm font-bold"
+      >
+        Download Template
+      </button>
+    </div>
+  );
 };
 
 export default SdeSlr;
