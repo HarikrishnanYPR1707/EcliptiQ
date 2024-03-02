9e52f48b0812fb3a6730cac125f3d8f1c90da2a4
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-02 19:32:57 +0530

    Added structure

--- src/components/resumeTemplates/da/DaElr.jsx
+++ src/components/resumeTemplates/da/DaElr.jsx
@@ -28,185 +28,185 @@ const DaElr = () => {
       <div className="flex flex-col items-center gap-5">
         <div
           ref={daElrComponentDownloadRef}
-          className="min-h-[1131px] w-full max-w-[800px] bg-white p-5 font-geologica text-black"
+          className="mt-5 flex min-h-[1131px] w-full max-w-[800px] items-start justify-between gap-5 bg-white p-5 font-geologica text-black"
         >
-          {/* resume header section  */}
-          <div className="">
+          {/* --------------------- left section --------------------- */}
+          <div className="w-[40%]">
+            {/* resume header section  */}
             <div className="">
-              <h1 className="pb-1 pt-6 text-3xl font-black">
-                Harikrishnan Prasannakumar
-              </h1>
-              <h3 className="text-2xl font-bold text-[#ff0505]">
-                Software Engineer
-              </h3>
+              <div className="">
+                <h1 className="pb-1 pt-6 text-3xl font-black">
+                  Harikrishnan Prasannakumar
+                </h1>
+                <h3 className="text-2xl font-bold text-[#ff0505]">
+                  Software Engineer
+                </h3>
+              </div>
+              <div className="flex flex-col items-start">
+                <p className="flex w-fit items-center justify-center gap-2 text-sm text-gray-500">
+                  <FaPhone className="text-gray-500" />
+                  <span className="">1234567890</span>
+                </p>
+                <p className="flex w-fit items-center justify-center gap-2 text-sm text-gray-500">
+                  <MdEmail className="text-gray-500" />
+                  <span className="">sample@gmail.com</span>
+                </p>
+                <p className="flex w-fit items-center justify-center text-sm text-gray-500">
+                  <a
+                    href="#"
+                    className="flex items-center justify-center gap-2"
+                  >
+                    <CgWebsite className="text-gray-500" />
+                    www.sample.com
+                  </a>
+                </p>
+                <p className="flex w-fit items-center justify-center text-sm text-gray-500">
+                  <a href="" className="flex items-center justify-center gap-2">
+                    <FaLinkedin className="" />
+                    <span className="text-blue-500">linkedin</span>
+                  </a>
+                </p>
+              </div>
             </div>
-            <div className="flex items-center justify-between">
-              <p className="flex w-fit items-center justify-center gap-2 text-sm text-gray-500">
-                <FaPhone className="text-gray-500" />
-                <span className="">1234567890</span>
-              </p>
-              <p className="flex w-fit items-center justify-center gap-2 text-sm text-gray-500">
-                <MdEmail className="text-gray-500" />
-                <span className="">sample@gmail.com</span>
-              </p>
-              <p className="flex w-fit items-center justify-center text-sm text-gray-500">
-                <a href="#" className="flex items-center justify-center gap-2">
-                  <CgWebsite className="text-gray-500" />
-                  www.sample.com
-                </a>
-              </p>
-              <p className="flex w-fit items-center justify-center text-sm text-gray-500">
-                <a href="" className="flex items-center justify-center gap-2">
-                  <FaLinkedin className="" />
-                  <span className="text-blue-500">linkedin</span>
-                </a>
-              </p>
+            {/* Education Section */}
+            <div className="mb-5">
+              <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
+                Education
+              </h1>
+              <div className="">
+                {/* header section */}
+                <h1 className=" text-[15px]">
+                  <span className="font-bold text-yellow-900">
+                    University of Mumbai
+                  </span>{" "}
+                  {" - "}
+                  <span className="">B.Tech in Computer Engineering</span>
+                </h1>
+                {/* Date and place section */}
+                <div className="flex items-center justify-between text-sm text-gray-500">
+                  <p className="">
+                    <span className="">January 2021</span>
+                    {" - "}
+                    <span className="">May 2024</span>
+                  </p>
+                  <p className="">Vasia, Kaman</p>
+                </div>
+              </div>
+            </div>
+            {/* Skills section  */}
+            <div className="">
+              <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
+                Skills
+              </h1>
+              <div className="">
+                {[
+                  "JavaScript",
+                  "HTML5",
+                  "CSS3",
+                  "React.js",
+                  "Node.js",
+                  "Express.js",
+                  "MongoDB",
+                  "SQL",
+                  "Git",
+                  "Webpack",
+                  "Babel",
+                  "RESTful APIs",
+                  "Responsive Web Design",
+                  "Testing (Jest, Mocha, Chai)",
+                  "Debugging",
+                  "Agile Methodologies",
+                  "Problem Solving",
+                  "Critical Thinking",
+                  "Communication",
+                  "Teamwork",
+                ].map((skill, i) => (
+                  <span key={i} className="text-sm">
+                    {skill + " "},
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
-          {/* bottom section */}
-          <div className="mt-5 flex items-start justify-between gap-5">
-            {/* --------------------- left section --------------------- */}
-            <div className="w-[60%]">
-              {/* Work section */}
+          {/* --------------------- right section --------------------- */}
+          <div className="flex w-[60%] flex-col items-start gap-5">
+            {/* Career Objective section  */}
+            <div className="">
+              <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
+                Career Objective
+              </h1>
+              <div className="text-sm">
+                Dynamic and results-driven professional with [X years] of
+                experience in [industry/field], demonstrating a strong track
+                record of [specific achievements or skills]. Skilled in
+                [relevant skills or technologies], with a passion for [specific
+                aspect of the industry]. Proven ability to [mention any notable
+                accomplishments or contributions].
+              </div>
+            </div>
+            {/* Work section */}
+            <div className="">
+              <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
+                Work Experience
+              </h1>
               <div className="">
-                <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
-                  Work Experience
-                </h1>
+                {/* experience-1 */}
                 <div className="">
-                  {/* experience-1 */}
-                  <div className="">
-                    {/* header section */}
-                    <h1 className="flex flex-col-reverse justify-start font-roboto text-[18px]">
-                      <span className="-mt-1 font-bold text-[#ff0505]">
-                        Coding Adda
-                      </span>
-                      <span className="text-[22px]">
-                        Senior Software Developer
-                      </span>
-                    </h1>
-                    {/* Date and place section */}
-                    <div className="flex items-center justify-between text-gray-500">
-                      <p className="">
-                        <span className="">June 2021</span>
-                        {" - "}
-                        <span className="">current</span>
-                      </p>
-                      <p className="">India</p>
-                    </div>
-                    {/* description section - 1 */}
-                    <ul className="ml-12 list-disc">
-                      {"Develop high-quality software solutions in a fast-paced environment, focusing on scalability, reliability, and performance.---Collaborate with cross-functional teams to gather requirements, design solutions, and implement features that meet business needs.---Write clean, efficient, and maintainable code using best practices and industry standards.---Conduct code reviews, provide constructive feedback, and mentor junior team members to foster a culture of continuous learning and improvement.---Troubleshoot and debug issues, identifying root causes and implementing timely resolutions to ensure smooth operation of software systems.---Stay up-to-date with emerging technologies, trends, and best practices in software development, and proactively contribute ideas for process improvements and innovation.---Participate in agile ceremonies such as sprint planning, daily stand-ups, and retrospectives to ensure alignment and transparency across teams.---Communicate effectively with stakeholders to gather requirements, provide updates on project progress, and address any concerns or questions.---Adhere to software development methodologies, coding standards, and version control practices to maintain code quality and project consistency.---Contribute to the overall success of the engineering team by sharing knowledge, collaborating on projects, and supporting a positive and inclusive work environment."
-                        .split("---")
-                        .map((item, i) => (
-                          <li key={i} className="text-sm">
-                            {item}
-                          </li>
-                        ))}
-                    </ul>
+                  {/* header section */}
+                  <h1 className="flex flex-col-reverse justify-start font-roboto text-[18px]">
+                    <span className="-mt-1 font-bold text-[#ff0505]">
+                      Coding Adda
+                    </span>
+                    <span className="text-[22px]">
+                      Senior Software Developer
+                    </span>
+                  </h1>
+                  {/* Date and place section */}
+                  <div className="flex items-center justify-between text-gray-500">
+                    <p className="">
+                      <span className="">June 2021</span>
+                      {" - "}
+                      <span className="">current</span>
+                    </p>
+                    <p className="">India</p>
                   </div>
+                  {/* description section - 1 */}
+                  <ul className="ml-12 list-disc">
+                    {"Develop high-quality software solutions in a fast-paced environment, focusing on scalability, reliability, and performance.---Collaborate with cross-functional teams to gather requirements, design solutions, and implement features that meet business needs.---Write clean, efficient, and maintainable code using best practices and industry standards.---Conduct code reviews, provide constructive feedback, and mentor junior team members to foster a culture of continuous learning and improvement.---Troubleshoot and debug issues, identifying root causes and implementing timely resolutions to ensure smooth operation of software systems.---Stay up-to-date with emerging technologies, trends, and best practices in software development, and proactively contribute ideas for process improvements and innovation.---Participate in agile ceremonies such as sprint planning, daily stand-ups, and retrospectives to ensure alignment and transparency across teams.---Communicate effectively with stakeholders to gather requirements, provide updates on project progress, and address any concerns or questions.---Adhere to software development methodologies, coding standards, and version control practices to maintain code quality and project consistency.---Contribute to the overall success of the engineering team by sharing knowledge, collaborating on projects, and supporting a positive and inclusive work environment."
+                      .split("---")
+                      .map((item, i) => (
+                        <li key={i} className="text-sm">
+                          {item}
+                        </li>
+                      ))}
+                  </ul>
                 </div>
               </div>
             </div>
-            {/* --------------------- right section --------------------- */}
-            <div className="flex w-[40%] flex-col items-start gap-5">
-              {/* Career Objective section  */}
+            {/* Relevant Courses section  */}
+            <div className="">
+              <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
+                Relevant Courses
+              </h1>
               <div className="">
-                <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
-                  Career Objective
-                </h1>
-                <div className="text-sm">
-                  Dynamic and results-driven professional with [X years] of
-                  experience in [industry/field], demonstrating a strong track
-                  record of [specific achievements or skills]. Skilled in
-                  [relevant skills or technologies], with a passion for
-                  [specific aspect of the industry]. Proven ability to [mention
-                  any notable accomplishments or contributions].
-                </div>
-              </div>
-              {/* Education Section */}
-              <div className="mb-5">
-                <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
-                  Education
-                </h1>
-                <div className="">
-                  {/* header section */}
-                  <h1 className=" text-[15px]">
-                    <span className="font-bold text-yellow-900">
-                      University of Mumbai
-                    </span>{" "}
-                    {" - "}
-                    <span className="">B.Tech in Computer Engineering</span>
-                  </h1>
-                  {/* Date and place section */}
-                  <div className="flex items-center justify-between text-sm text-gray-500">
-                    <p className="">
-                      <span className="">January 2021</span>
-                      {" - "}
-                      <span className="">May 2024</span>
-                    </p>
-                    <p className="">Vasia, Kaman</p>
-                  </div>
-                </div>
-              </div>
-              {/* Relevant Courses section  */}
-              <div className="">
-                <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
-                  Relevant Courses
-                </h1>
-                <div className="">
-                  {[
-                    "JavaScript Fundamentals",
-                    "Advanced JavaScript",
-                    "Front-End Web Development",
-                    "Back-End Web Development",
-                    "React.js Essentials",
-                    "Node.js Basics",
-                    "Express.js Fundamentals",
-                    "MongoDB for Beginners",
-                    "SQL Fundamentals",
-                    "Full-Stack Web Development",
-                    "Responsive Web Design",
-                    "Web Development Bootcamp",
-                  ].map((skill, i) => (
-                    <span key={i} className="text-sm">
-                      {skill + " "},
-                    </span>
-                  ))}
-                </div>
-              </div>
-              {/* Skills section  */}
-              <div className="">
-                <h1 className="mb-2 border-b-[3px] border-black font-roboto text-2xl font-black uppercase">
-                  Skills
-                </h1>
-                <div className="">
-                  {[
-                    "JavaScript",
-                    "HTML5",
-                    "CSS3",
-                    "React.js",
-                    "Node.js",
-                    "Express.js",
-                    "MongoDB",
-                    "SQL",
-                    "Git",
-                    "Webpack",
-                    "Babel",
-                    "RESTful APIs",
-                    "Responsive Web Design",
-                    "Testing (Jest, Mocha, Chai)",
-                    "Debugging",
-                    "Agile Methodologies",
-                    "Problem Solving",
-                    "Critical Thinking",
-                    "Communication",
-                    "Teamwork",
-                  ].map((skill, i) => (
-                    <span key={i} className="text-sm">
-                      {skill + " "},
-                    </span>
-                  ))}
-                </div>
+                {[
+                  "JavaScript Fundamentals",
+                  "Advanced JavaScript",
+                  "Front-End Web Development",
+                  "Back-End Web Development",
+                  "React.js Essentials",
+                  "Node.js Basics",
+                  "Express.js Fundamentals",
+                  "MongoDB for Beginners",
+                  "SQL Fundamentals",
+                  "Full-Stack Web Development",
+                  "Responsive Web Design",
+                  "Web Development Bootcamp",
+                ].map((skill, i) => (
+                  <span key={i} className="text-sm">
+                    {skill + " "},
+                  </span>
+                ))}
               </div>
             </div>
           </div>
