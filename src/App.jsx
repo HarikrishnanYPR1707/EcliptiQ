6779e7daa76875cf62fdb29532a011475a06ed12
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-04 00:37:31 +0530

    Added route for BkJlr template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BkElr from "./components/resumeTemplates/banking/BkElr";
+import BkJlr from "./components/resumeTemplates/banking/BkJlr";
 import DaElr from "./components/resumeTemplates/da/DaElr";
 import DaJlr from "./components/resumeTemplates/da/DaJlr";
 import DaSlr from "./components/resumeTemplates/da/DaSlr";
@@ -29,7 +30,7 @@ const App = () => {
             <Route path="/templates/da/slr" element={<DaSlr />} />
             <Route path="/templates/bk/elr" element={<BkElr />} />
             {/* <Route path="/templates/bk/jlr" element={<DaJlr />} /> */}
-            {/* <Route path="/templates/bk/slr" element={<DaSlr />} /> */}
+            <Route path="/templates/bk/slr" element={<BkJlr />} />
             <Route path="*" element={<div>404 Page</div>} />
           </Routes>
         </div>
