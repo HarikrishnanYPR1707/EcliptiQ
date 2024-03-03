15526c906fc06f0ffabde1a505554d8c7772ae64
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-03 23:40:26 +0530

    Added basic structure for BK-ELR template

--- src/components/resumeTemplates/banking/BkElr.jsx
+++ src/components/resumeTemplates/banking/BkElr.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg";
+import { FaLinkedin } from "react-icons/fa";
 
 const BkElr = () => {
-  return <div>BkElr</div>;
+  const bkElrComponentDownloadRef = useRef(null);
+
+  const handlePrint = useReactToPrint({
+    content: () => bkElrComponentDownloadRef.current,
+    documentTitle: "SDE-SLR-Resume",
+    // pageStyle: "print",
+  });
+
+  return (
+    <div className="flex flex-col gap-10">
+      {/* ------------------ header section ------------------ */}
+      <div className="relative flex h-[200px] w-full items-center justify-center bg-banner bg-cover bg-center">
+        <div className="absolute inset-0 bg-black bg-opacity-60" />
+        <div className="relative flex flex-col items-center justify-center gap-4">
+          <h1 className="text-5xl font-black  uppercase">Banking</h1>
+          <h3 className="rounded-xl bg-purple-500 bg-opacity-80 px-20 py-2 text-xl">
+            Entry Level Resume
+          </h3>
+        </div>
+      </div>
+      {/* ------------------ template part with button ------------------ */}
+      <div className="flex flex-col items-center gap-5">
+        <div
+          ref={bkElrComponentDownloadRef}
+          className="min-h-[1131px] w-full max-w-[800px] bg-white font-geologica text-black"
+        >
+          <div className="flex w-full border border-purple-500">
+            {/* left section */}
+            <div className="min-h-[1131px] w-[35%] border border-blue-500 bg-[#f5f1ee]"></div>
+            {/* right section */}
+            <div className="min-h-[1131px] w-[75%] border border-red-500"></div>
+          </div>
+        </div>
+        <button
+          onClick={handlePrint}
+          className="rounded-lg bg-purple-500 px-3 py-2 text-sm font-bold"
+        >
+          Download Template
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BkElr;
