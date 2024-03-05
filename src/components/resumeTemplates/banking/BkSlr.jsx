326284ef7e825f8c045fffb0bf53d5893f1ba002
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-06 01:49:25 +0530

    Done with dynamic data updation in BK-SLR

--- src/components/resumeTemplates/banking/BkSlr.jsx
+++ src/components/resumeTemplates/banking/BkSlr.jsx
@@ -1,8 +1,13 @@
+import { userResumeData } from "@/Fetch/InformationFetch";
 import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 
 const BkSlr = () => {
   const bkElrComponentDownloadRef = useRef(null);
+
+  const data = userResumeData();
+  const resumeData = JSON.parse(data);
+  // console.log(resumeData);
 
   const handlePrint = useReactToPrint({
     content: () => bkElrComponentDownloadRef.current,
@@ -26,52 +31,58 @@ const BkSlr = () => {
       <div className="flex flex-col items-center gap-5">
         <div
           ref={bkElrComponentDownloadRef}
-          className="min-h-[1131px] w-full max-w-[800px] bg-white font-geologica text-black"
+          className="min-h-[1100px] w-full max-w-[800px] bg-white font-geologica text-black"
         >
           <div className="flex w-full">
             {/* --------------------- left section ---------------------*/}
-            <div className="min-h-[1131px] w-[35%] bg-[#f1f2f2] p-7 text-gray-500">
+            <div className="min-h-[1100px] w-[35%] bg-[#f1f2f2] p-7 text-gray-500">
               {/* header section */}
               <div className="">
                 <h1 className="flex flex-col text-3xl text-[#58595b]">
-                  <span className="">Harikrishnan</span>
-                  <span className="">Prasannakumar</span>
+                  <span className="">
+                    {resumeData.personalDetails.firstName}
+                  </span>
+                  <span className="">
+                    {resumeData.personalDetails.lastName}
+                  </span>
                 </h1>
                 <div className="mt-4 flex flex-col items-start justify-between">
                   <p className="flex w-fit items-center justify-center gap-2 text-sm text-gray-500">
-                    <span className="">1234567890</span>
+                    <span className="">
+                      {" "}
+                      {resumeData.personalDetails.phone}
+                    </span>
                   </p>
                   <p className="flex w-fit items-center justify-center gap-2 text-sm text-gray-500">
-                    <span className="">sample@gmail.com</span>
+                    <span className="">
+                      {" "}
+                      {resumeData.personalDetails.email}
+                    </span>
                   </p>
                   <p className="flex w-fit items-center justify-center text-sm text-gray-500">
                     <a
                       href="#"
                       className="flex items-center justify-center gap-2"
                     >
-                      www.sample.com
+                      {resumeData.personalDetails.website}
                     </a>
                   </p>
                   <p className="flex w-fit items-center justify-center text-sm text-gray-500">
                     <a
-                      href=""
+                      href={resumeData.socialLink.link1.link}
                       className="flex items-center justify-center gap-2"
                     >
-                      <span className="text-[#58595b]">linkedin</span>
+                      <span className="text-[#58595b]">
+                        {" "}
+                        {resumeData.socialLink.link1.label}
+                      </span>
                     </a>
                   </p>
                 </div>
               </div>
               {/* objective section */}
               <div className="mt-5 text-xs">
-                Highly skilled and results-oriented Front-End Engineer with X
-                years of experience in designing, developing, and implementing
-                user-centric web applications. Proven ability to deliver
-                high-quality, responsive, and performant websites and
-                applications across various platforms. Expertise in HTML, CSS,
-                JavaScript frameworks (e.g., React, Angular, Vue.js), and a
-                strong understanding of best practices for UI/UX design and
-                development.
+                {resumeData.professionalSummary}
               </div>
               {/* Skills section  */}
               <div className="mt-5">
@@ -79,22 +90,7 @@ const BkSlr = () => {
                   Key Skills
                 </h1>
                 <ul className="">
-                  {[
-                    "SQL",
-                    "Python",
-                    "R",
-                    "JavaScript",
-                    "Excel",
-                    "Data Cleaning & Wrangling",
-                    "Data Visualization (e.g., Tableau, Power BI)",
-                    "Statistical Analysis",
-                    "Machine Learning Fundamentals",
-                    "Communication",
-                    "Critical Thinking & Problem-Solving",
-                    "Storytelling",
-                    "Data Curiosity & Attention to Detail",
-                    "Collaboration & Teamwork",
-                  ].map((skill, i) => (
+                  {resumeData.skills.split("---").map((skill, i) => (
                     <li
                       key={i}
                       className="ml-5 list-disc text-sm text-gray-600"
@@ -112,43 +108,86 @@ const BkSlr = () => {
                 <h1 className="mb-2 text-xl font-normal text-[#58595b]">
                   Professional Experience
                 </h1>
-                {Array(2)
-                  .fill(true)
-                  .map((item, i) => (
-                    <div className="mb-3">
-                      {/* -------------------- experiences -------------------- */}
-                      <div className="">
-                        {/* header section */}
-                        <h1 className="flex flex-col-reverse justify-start font-roboto text-[18px]">
-                          <span className="text-base font-normal text-[#58595b]">
-                            Banking Associate
-                          </span>
-                        </h1>
-                        {/* Date and place section */}
-                        <div className="flex items-center justify-between text-sm italic text-gray-500">
-                          <p className="">
-                            <span className="">Bank of America</span>
-                            {", "}
-                            <span className="">San Francisco, CA</span>
-                            {" | "}
-                            <span className="">June 2021</span>
-                            {" - "}
-                            <span className="">current</span>
-                          </p>
-                        </div>
-                        {/* description section - 1 */}
-                        <ul className="ml-10 list-disc">
-                          {"Provided exceptional customer service: Assisted clients with a wide range of needs, including account opening, processing transactions, resolving inquiries, and offering personalized financial solutions.---Maintained accurate and secure financial records: Processed deposits, withdrawals, and other transactions with strict adherence to bank policies and procedures. Ensured data integrity and confidentiality at all times.---Cross-sold banking products and services: Identified client needs and proactively recommended appropriate products such as savings accounts, credit cards, and investment options. Achieved consistent sales targets.---Managed account relationships: Built rapport with clients, exceeding expectations by understanding their financial goals and providing ongoing support.---Adhered to all regulatory compliance requirements: Maintained awareness of and ensured adherence to KYC (Know Your Customer) and AML (Anti-Money Laundering) regulations."
-                            .split("---")
-                            .map((item, i) => (
-                              <li key={i} className="text-xs">
-                                {item}
-                              </li>
-                            ))}
-                        </ul>
-                      </div>
+                <div className="mb-3">
+                  {/* -------------------- experiences - 1 -------------------- */}
+                  <div className="">
+                    {/* header section */}
+                    <h1 className="flex flex-col-reverse justify-start font-roboto text-[18px]">
+                      <span className="text-base font-normal text-[#58595b]">
+                        {resumeData.experience.experience1.jobTitle}
+                      </span>
+                    </h1>
+                    {/* Date and place section */}
+                    <div className="flex items-center justify-between text-sm italic text-gray-500">
+                      <p className="">
+                        <span className="">
+                          {resumeData.experience.experience1.employer}
+                        </span>
+                        {", "}
+                        <span className="">
+                          {resumeData.experience.experience1.place}
+                        </span>
+                        {" | "}
+                        <span className="">
+                          {resumeData.experience.experience1.startDate}
+                        </span>
+                        {" - "}
+                        <span className="">
+                          {resumeData.experience.experience1.endDate}
+                        </span>
+                      </p>
                     </div>
-                  ))}
+                    {/* description section - 1 */}
+                    <ul className="ml-10 list-disc">
+                      {resumeData.experience.experience1.description
+                        .split("---")
+                        .map((item, i) => (
+                          <li key={i} className="text-xs">
+                            {item}
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
+                  {/* -------------------- experiences - 2 -------------------- */}
+                  <div className="mt-5">
+                    {/* header section */}
+                    <h1 className="flex flex-col-reverse justify-start font-roboto text-[18px]">
+                      <span className="text-base font-normal text-[#58595b]">
+                        {resumeData.experience.experience2.jobTitle}
+                      </span>
+                    </h1>
+                    {/* Date and place section */}
+                    <div className="flex items-center justify-between text-sm italic text-gray-500">
+                      <p className="">
+                        <span className="">
+                          {resumeData.experience.experience2.employer}
+                        </span>
+                        {", "}
+                        <span className="">
+                          {resumeData.experience.experience2.place}
+                        </span>
+                        {" | "}
+                        <span className="">
+                          {resumeData.experience.experience2.startDate}
+                        </span>
+                        {" - "}
+                        <span className="">
+                          {resumeData.experience.experience2.endDate}
+                        </span>
+                      </p>
+                    </div>
+                    {/* description section - 1 */}
+                    <ul className="ml-10 list-disc">
+                      {resumeData.experience.experience2.description
+                        .split("---")
+                        .map((item, i) => (
+                          <li key={i} className="text-xs">
+                            {item}
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
+                </div>
               </div>
               {/* education section  */}
               <div className="mt-5">
@@ -158,19 +197,19 @@ const BkSlr = () => {
                 <div className="">
                   {/* header section */}
                   <h1 className="flex flex-col-reverse text-[15px]">
-                    <span className="">University of Mumbai</span>
+                    <span className="">{resumeData.education.college}</span>
                     <span className="text-[#58595b]">
-                      B.Tech in Computer Engineering
+                      {resumeData.education.degree}
                     </span>
                   </h1>
                   {/* Date and place section */}
                   <div className="flex flex-col items-start justify-between text-sm text-gray-500">
                     <p className="">
-                      <span className="">January 2021</span>
+                      <span className="">{resumeData.education.startDate}</span>
                       {" - "}
-                      <span className="">May 2024</span>
+                      <span className="">{resumeData.education.endDate}</span>
                     </p>
-                    <p className="">Vasai, Kaman</p>
+                    <p className="">{resumeData.education.place}</p>
                   </div>
                 </div>
               </div>
