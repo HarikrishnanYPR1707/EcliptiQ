7cc6b126902ce25bf8ae5e2763bf647f5ba66094
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-05 20:28:59 +0530

    Added Changes

--- src/Fetch/InformationFetch.js
+++ src/Fetch/InformationFetch.js
@@ -1,3 +1,50 @@
 export const userResumeData = () => {
   return localStorage.getItem("userResumeData");
 };
+
+// {
+//   personalDetails: {
+//     jobTitle: "Frontend Developer",
+//     firstName: "Harikrishnan",
+//     lastName: "Prasannakumar",
+//     email: "hk17.dev@gmail.com",
+//     phone: "7776861386",
+//   },
+//   professionalSummary: "",
+//   education: {
+//     college: "Universal College of Enginereing",
+//     degree: "B.Tech in Computer Engineering",
+//     startDate: "2021-07-17",
+//     endDate: "2024-07-17",
+//     description:
+//       "University Name: Universal College of Engineering---Degree: B.Tech in Computer Engineering---Expected Graduation Date: July 2024---GPA (Optional): Include only if above 3.5 or within the honor roll range (e.g., July 2024 (Expected), GPA: 3.8)---Relevant Coursework (Optional): List a few key courses aligned with your target job. (e.g., Data Structures & Algorithms, Software Design Patterns, Computer Networks)---Honors & Awards (Optional): Mention any relevant academic achievements like scholarships, dean's list mentions, or competition awards.---Projects/Activities (Optional): Briefly describe any academic projects or extracurricular activities related to computer science that showcase your skills and interests. (e.g., Senior Capstone Project: Developed a recommendation system using machine learning)---Study Abroad (Optional): If applicable, mention any international study programs you participated in, highlighting the location and duration.",
+//   },
+//   experience: {
+//     jobTitle: "Software Development Engineer - 3",
+//     employer: "Google",
+//     startDate: "2021-07-17",
+//     endDate: "2024-07-17",
+//     description:
+//       "SDE-2 is also commonly addressed as a Senior Software Engineer or Senior Member of Technical Staff in different organizations.---An SDE-2 by capacity is an individual contributor who works in a team.---When someone is hired as an SDE-2 or gets promoted to an SDE-2 role, it is expected that they have considerable experience and won't need much help in solving straightforward issues.---A Software Development Engineer-2 (or an E4 in the case of Meta) holds end-to-end service ownership in various product-based organizations. Owning a service means being fully aware of everything that goes into it, even if you don't physically do the coding.---They’re tasked with providing rational, tested, efficient, and maintainable solutions to challenging software issues.---They’re also in charge of training new team members on the clients, what the team's software does, how it is built, tested, used, and how it fits into the overall picture.",
+//   },
+//   socialLink: {
+//     link1: {
+//       label: "twitter",
+//       link: "twitter.com",
+//     },
+//     link2: {
+//       label: "linkedin",
+//       link: "linkedin.com",
+//     },
+//     link3: {
+//       label: "youtube",
+//       link: "youtube.com",
+//     },
+//     link4: {
+//       label: "behance",
+//       link: "behance.com",
+//     },
+//   },
+//   skills: "ReactJs---NextJs---TailwindCSS---Framer Motion---GSAP-SCSS",
+//   language: "Malayalam---English---Hindi---Marathi",
+// }
