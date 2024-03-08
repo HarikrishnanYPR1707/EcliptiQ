21dd12e629f388c72775390ad04a203ccdafe521
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-08 20:54:03 +0530

    Added Changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
+const cors = require("cors");
 const port = 8000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.send("Hello from home page");
+});
+app.post("/api/add/userResume", (req, res) => {
+  try {
+    const data = req.body;
+
+    console.log(data);
+
+    res.send("done");
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 app.listen(port, () => console.log("SERVER STARTED!!"));
