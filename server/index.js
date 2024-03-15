537e84ad75bf2b1e333f38a163519cdd3b943ec0
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-15 07:06:47 +0530

    removed imports

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const sampleHomePage = require("./routes/sample.routes.js");
 const userResumeData = require("./routes/user.routes.js");
 
 const port = 8000;
@@ -14,8 +13,5 @@ app.use("/api", userResumeData);
 app.use("/", (req, res) => {
   res.send("Server is Up and Running!!");
 });
-
-// app.use(sampleHomePage);
-// app.use(userResumeData);
 
 app.listen(port, () => console.log("SERVER STARTED!!"));
