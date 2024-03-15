5838d0d38fd85a55352f8d04235eebb528575fbc
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-15 07:06:26 +0530

    Deleted files

--- server/controllers/sampleController.js
+++
@@ -1,9 +0,0 @@
-const home = async (req, res) => {
-  try {
-    res.status(200).send("Harikrishnan Prasnnakumar");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-module.exports = { home };
