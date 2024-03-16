45ba3a94fef66534fac46a1d91d67a1e4beb3ba1
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-17 00:01:41 +0530

    Added Changes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {
-  addUserData,
+  addUserDataa,
   // getUserData,
 } = require("../controllers/userResume.controller.js");
 
