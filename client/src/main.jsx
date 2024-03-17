835bad5bda15a9294e16397aeb682701f6b3795e
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-17 20:51:53 +0530

    Added Strictmode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -7,7 +7,9 @@ import { ThemeProvider } from "./components/theme-provider.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <StrictMode>
+        <App />
+      </StrictMode>
     </ThemeProvider>
   </React.StrictMode>,
 );
