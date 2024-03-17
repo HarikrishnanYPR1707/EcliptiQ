aeecc2e272598025076a407dda10550116727db3
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-17 20:45:24 +0530

    imported userEmailContext and removed unused code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,97 +10,37 @@ import SdeElr from "./components/resumeTemplates/sde/SdeElr";
 import SdeJlr from "./components/resumeTemplates/sde/SdeJlr";
 import SdeSlr from "./components/resumeTemplates/sde/SdeSlr";
 import Home from "./pages/Home";
-import Information1 from "./pages/Information";
 import Template from "./pages/Template";
 import Information from "./pages/Information";
 import KeyWords from "./pages/KeyWords";
-// import { useState } from "react";
-// import { InformationContext } from "./Contexts/InformationContext";
+import { userEmailContext } from "./Contexts/userEmailContext";
 
 const App = () => {
-  // const [userResumeData, setUserResumeData] = useState({
-  //   personalDetails: {
-  //     jobTitle: "",
-  //     firstName: "",
-  //     lastName: "",
-  //     email: "",
-  //     phone: "",
-  //   },
-  //   professionalSummary: "",
-  //   education: {
-  //     college: "",
-  //     degree: "",
-  //     startDate: "",
-  //     endDate: "",
-  //     description: "",
-  //   },
-  //   experience: {
-  //     jobTitle: "",
-  //     employer: "",
-  //     startDate: "",
-  //     endDate: "",
-  //     description: "",
-  //   },
-  //   socialLink: {
-  //     link1: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //     link2: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //     link3: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //     link4: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //   },
-  //   skills: {
-  //     skill1: "",
-  //     skill2: "",
-  //     skill3: "",
-  //     skill4: "",
-  //     skill5: "",
-  //     skill6: "",
-  //   },
-  //   language: {
-  //     lang1: "",
-  //     lang2: "",
-  //     lang3: "",
-  //   },
-  // });
-
   return (
     <>
-      {/* <InformationContext.Provider
-        value={{ userResumeData, setUserResumeData }}
-      > */}
-      <Router>
-        <Navbar />
-        <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/templates" element={<Template />} />
-            <Route path="/information" element={<Information />} />
-            <Route path="/keyword" element={<KeyWords />} />
-            <Route path="/templates/sde/elr" element={<SdeElr />} />
-            <Route path="/templates/sde/jlr" element={<SdeJlr />} />
-            <Route path="/templates/sde/slr" element={<SdeSlr />} />
-            <Route path="/templates/da/elr" element={<DaElr />} />
-            <Route path="/templates/da/jlr" element={<DaJlr />} />
-            <Route path="/templates/da/slr" element={<DaSlr />} />
-            <Route path="/templates/bk/elr" element={<BkElr />} />
-            <Route path="/templates/bk/jlr" element={<BkJlr />} />
-            <Route path="/templates/bk/slr" element={<BkSlr />} />
-            <Route path="*" element={<div>404 Page</div>} />
-          </Routes>
-        </div>
-      </Router>
-      {/* </InformationContext.Provider> */}
+      <userEmailContext.Provider value={{ userResumeData, setUserResumeData }}>
+        <Router>
+          <Navbar />
+          <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/templates" element={<Template />} />
+              <Route path="/information" element={<Information />} />
+              <Route path="/keyword" element={<KeyWords />} />
+              <Route path="/templates/sde/elr" element={<SdeElr />} />
+              <Route path="/templates/sde/jlr" element={<SdeJlr />} />
+              <Route path="/templates/sde/slr" element={<SdeSlr />} />
+              <Route path="/templates/da/elr" element={<DaElr />} />
+              <Route path="/templates/da/jlr" element={<DaJlr />} />
+              <Route path="/templates/da/slr" element={<DaSlr />} />
+              <Route path="/templates/bk/elr" element={<BkElr />} />
+              <Route path="/templates/bk/jlr" element={<BkJlr />} />
+              <Route path="/templates/bk/slr" element={<BkSlr />} />
+              <Route path="*" element={<div>404 Page</div>} />
+            </Routes>
+          </div>
+        </Router>
+      </userEmailContext.Provider>
     </>
   );
 };
