4e3280d51226ad8573c98cc9c4e5247bf032b00f
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-20 21:41:14 +0530

    Added button styles

--- client/src/pages/KeyWords.jsx
+++ client/src/pages/KeyWords.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   sdeKeywordData,
   gdKeywordData,
@@ -7,6 +7,7 @@ import {
 } from "@/Data/Keyword";
 
 const KeyWords = () => {
+  const [domain, setDomain] = useState();
   return (
     <div className="flex flex-col items-center justify-center border-white pb-5">
       {/* image container */}
@@ -19,23 +20,39 @@ const KeyWords = () => {
         </p>
       </div>
       {/* keyword section */}
-      {/* sde section */}
-      <div className="">
-        <div className="relative my-5 flex h-[70px] w-[850px] items-center justify-center bg-banner bg-cover bg-center underline">
-          <div className="absolute inset-0 bg-black bg-opacity-60" />
-          <h1 className="relative text-center text-2xl font-black sm:text-3xl lg:text-3xl">
-            Software Development Engineer
-          </h1>
+      <div className="w-[850px]">
+        <div className="flex items-center justify-center gap-5 border border-purple-500">
+          <button className="rounded-xl border border-rose-500 bg-purple-500 px-5 py-2 font-bold">
+            Software Engineer
+          </button>
+          <button className="rounded-xl border border-rose-500 bg-purple-500 px-5 py-2 font-bold">
+            Graphic Designer
+          </button>
+          <button className="rounded-xl border border-rose-500 bg-purple-500 px-5 py-2 font-bold">
+            Management
+          </button>
+          <button className="rounded-xl border border-rose-500 bg-purple-500 px-5 py-2 font-bold">
+            Data Analyst
+          </button>
         </div>
-        <div className="grid grid-cols-2 gap-5">
-          {sdeKeywordData.map((item, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-center rounded-xl border border-gray-700 px-5 py-2"
-            >
-              {item}
-            </div>
-          ))}
+        {/* sde section */}
+        <div className="border border-rose-500">
+          <div className="relative my-5 flex h-[70px] items-center justify-center bg-banner bg-cover bg-center underline">
+            <div className="absolute inset-0 bg-black bg-opacity-60" />
+            <h1 className="relative text-center text-2xl font-black sm:text-3xl lg:text-3xl">
+              Software Development Engineer
+            </h1>
+          </div>
+          <div className="grid grid-cols-2 gap-5">
+            {sdeKeywordData.map((item, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-center rounded-xl border border-gray-700 px-5 py-2"
+              >
+                {item}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
