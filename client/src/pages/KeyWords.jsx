6429b6dd038ae1f918e1c111cb683fda281d1d79
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-03-21 07:22:49 +0530

    passed domain prop for mapping data dynamically

--- client/src/pages/KeyWords.jsx
+++ client/src/pages/KeyWords.jsx
@@ -7,7 +7,7 @@ import {
 } from "@/Data/Keyword";
 
 const KeyWords = () => {
-  const [domain, setDomain] = useState("");
+  const [domain, setDomain] = useState(sdeKeywordData);
 
   return (
     <div className="flex flex-col items-center justify-center border-white pb-5">
@@ -23,37 +23,53 @@ const KeyWords = () => {
       {/* keyword section */}
       <div className="w-[850px]">
         <div className="my-5 flex items-center justify-center gap-5">
-          <button className="rounded-lg bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600">
+          <button
+            onClick={() => setDomain(sdeKeywordData)}
+            className="rounded-lg bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600"
+          >
             Software Development Engineer
           </button>
-          <button className="rounded-xl bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600">
+          <button
+            onClick={() => setDomain(gdKeywordData)}
+            className="rounded-xl bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600"
+          >
             Graphic Designer
           </button>
-          <button className="rounded-xl bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600">
+          <button
+            onClick={() => setDomain(mgnKeywordData)}
+            className="rounded-xl bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600"
+          >
             Management
           </button>
-          <button className="rounded-xl bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600">
+          <button
+            onClick={() => setDomain(daKeywordData)}
+            className="rounded-xl bg-purple-500 px-5 py-2 font-bold hover:bg-purple-600"
+          >
             Data Analyst
           </button>
         </div>
         {/* sde section */}
-        <Component />
+        <Component domain={domain} />
       </div>
     </div>
   );
 };
 
-const Component = (props) => {
+const Component = ({ domain }) => {
+  // console.log(domain);
+  domain.map((item) => {
+    console.log(item);
+  });
   return (
     <div className="border border-rose-500">
       <div className="relative my-5 flex h-[70px] items-center justify-center bg-banner bg-cover bg-center underline">
         <div className="absolute inset-0 bg-black bg-opacity-60" />
         <h1 className="relative text-center text-2xl font-black sm:text-3xl lg:text-3xl">
-          Software Development Engineer
+          {/* {props.domain} */}
         </h1>
       </div>
       <div className="grid grid-cols-2 gap-5">
-        {sdeKeywordData.map((item, index) => (
+        {domain.map((item, index) => (
           <div
             key={index}
             className="flex items-center justify-center rounded-xl border border-gray-700 px-5 py-2"
