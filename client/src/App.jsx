145314bafdd5bbdc8a87010b1befc4dfac848280
Harikrishnan17 <hyprgd2002@gmail.com>
2024-03-27 22:34:25 +0530

    Used provider for passing user login context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,36 +13,42 @@ import Home from "./pages/Home";
 import Template from "./pages/Template";
 import Information from "./pages/Information";
 import KeyWords from "./pages/KeyWords";
-import { userDataContext } from "./Contexts/userDataContext";
+import {
+  userDataContext,
+  isUserLoginContext,
+} from "./Contexts/userDataContext";
 import { useState } from "react";
 
 const App = () => {
   const [user, setUser] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <>
-      <userDataContext.Provider value={{ user, setUser }}>
-        <Router>
-          <Navbar />
-          <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/templates" element={<Template />} />
-              <Route path="/information" element={<Information />} />
-              <Route path="/keyword" element={<KeyWords />} />
-              <Route path="/templates/sde/elr" element={<SdeElr />} />
-              <Route path="/templates/sde/jlr" element={<SdeJlr />} />
-              <Route path="/templates/sde/slr" element={<SdeSlr />} />
-              <Route path="/templates/da/elr" element={<DaElr />} />
-              <Route path="/templates/da/jlr" element={<DaJlr />} />
-              <Route path="/templates/da/slr" element={<DaSlr />} />
-              <Route path="/templates/bk/elr" element={<BkElr />} />
-              <Route path="/templates/bk/jlr" element={<BkJlr />} />
-              <Route path="/templates/bk/slr" element={<BkSlr />} />
-              <Route path="*" element={<div>404 Page</div>} />
-            </Routes>
-          </div>
-        </Router>
-      </userDataContext.Provider>
+      <isUserLoginContext.Provider value={{ isLogin, setIsLogin }}>
+        <userDataContext.Provider value={{ user, setUser }}>
+          <Router>
+            <Navbar />
+            <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/templates" element={<Template />} />
+                <Route path="/information" element={<Information />} />
+                <Route path="/keyword" element={<KeyWords />} />
+                <Route path="/templates/sde/elr" element={<SdeElr />} />
+                <Route path="/templates/sde/jlr" element={<SdeJlr />} />
+                <Route path="/templates/sde/slr" element={<SdeSlr />} />
+                <Route path="/templates/da/elr" element={<DaElr />} />
+                <Route path="/templates/da/jlr" element={<DaJlr />} />
+                <Route path="/templates/da/slr" element={<DaSlr />} />
+                <Route path="/templates/bk/elr" element={<BkElr />} />
+                <Route path="/templates/bk/jlr" element={<BkJlr />} />
+                <Route path="/templates/bk/slr" element={<BkSlr />} />
+                <Route path="*" element={<div>404 Page</div>} />
+              </Routes>
+            </div>
+          </Router>
+        </userDataContext.Provider>
+      </isUserLoginContext.Provider>
     </>
   );
 };
