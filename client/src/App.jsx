1e5118613cd5ab45335dcd9175b2b01d464d4406
Harikrishnan17 <hyprgd2002@gmail.com>
2024-03-29 21:21:39 +0530

    Added context provider for resume context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,38 +17,44 @@ import {
   userEmailContext,
   isUserLoginContext,
 } from "./Contexts/userDataContext";
+import { currentResumeContext } from "./Contexts/ResumeContext";
 import { useState } from "react";
 
 const App = () => {
   const [user, setUser] = useState("");
   const [isLogin, setIsLogin] = useState(false);
+  const [currentResumeId, setCurrentResumeId] = useState();
   return (
     <>
-      <isUserLoginContext.Provider value={{ isLogin, setIsLogin }}>
-        <userEmailContext.Provider value={{ user, setUser }}>
-          <Router>
-            <Navbar />
-            <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/templates" element={<Template />} />
-                <Route path="/information/:id" element={<Information />} />
-                <Route path="/keyword" element={<KeyWords />} />
-                <Route path="/templates/sde/elr" element={<SdeElr />} />
-                <Route path="/templates/sde/jlr" element={<SdeJlr />} />
-                <Route path="/templates/sde/slr" element={<SdeSlr />} />
-                <Route path="/templates/da/elr" element={<DaElr />} />
-                <Route path="/templates/da/jlr" element={<DaJlr />} />
-                <Route path="/templates/da/slr" element={<DaSlr />} />
-                <Route path="/templates/bk/elr" element={<BkElr />} />
-                <Route path="/templates/bk/jlr" element={<BkJlr />} />
-                <Route path="/templates/bk/slr" element={<BkSlr />} />
-                <Route path="*" element={<div>404 Page</div>} />
-              </Routes>
-            </div>
-          </Router>
-        </userEmailContext.Provider>
-      </isUserLoginContext.Provider>
+      <currentResumeContext.Provider
+        value={{ currentResumeId, setCurrentResumeId }}
+      >
+        <isUserLoginContext.Provider value={{ isLogin, setIsLogin }}>
+          <userEmailContext.Provider value={{ user, setUser }}>
+            <Router>
+              <Navbar />
+              <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/templates" element={<Template />} />
+                  <Route path="/information/:id" element={<Information />} />
+                  <Route path="/keyword" element={<KeyWords />} />
+                  <Route path="/templates/sde/elr" element={<SdeElr />} />
+                  <Route path="/templates/sde/jlr" element={<SdeJlr />} />
+                  <Route path="/templates/sde/slr" element={<SdeSlr />} />
+                  <Route path="/templates/da/elr" element={<DaElr />} />
+                  <Route path="/templates/da/jlr" element={<DaJlr />} />
+                  <Route path="/templates/da/slr" element={<DaSlr />} />
+                  <Route path="/templates/bk/elr" element={<BkElr />} />
+                  <Route path="/templates/bk/jlr" element={<BkJlr />} />
+                  <Route path="/templates/bk/slr" element={<BkSlr />} />
+                  <Route path="*" element={<div>404 Page</div>} />
+                </Routes>
+              </div>
+            </Router>
+          </userEmailContext.Provider>
+        </isUserLoginContext.Provider>
+      </currentResumeContext.Provider>
     </>
   );
 };
