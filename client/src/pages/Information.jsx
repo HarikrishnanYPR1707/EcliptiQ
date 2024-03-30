97b6e4eed2bba8c40fb825a680c46b1788ee5d83
Harikrishnan17 <hyprgd2002@gmail.com>
2024-03-30 23:39:54 +0530

    Added fetch data function for fetching data using ID which is obtained form useParams

--- client/src/pages/Information.jsx
+++ client/src/pages/Information.jsx
@@ -2,7 +2,7 @@ import { currentResumeContext } from "@/Contexts/ResumeContext.js";
 import { userEmailContext } from "@/Contexts/userDataContext.js";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { FaInfoCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -10,197 +10,203 @@ import { BASEURL } from "../assets/API/api.js";
 import { useParams } from "react-router-dom";
 
 const Information = () => {
-  // const [userResumeData, setUserResumeData] = useState({
-  //   fileName: "",
-  //   personalDetails: {
-  //     jobTitle: "",
-  //     firstName: "",
-  //     lastName: "",
-  //     email: "",
-  //     phone: "",
-  //     website: "",
-  //   },
-  //   professionalSummary: "",
-  //   education: {
-  //     college: "",
-  //     degree: "",
-  //     place: "",
-  //     startDate: "",
-  //     endDate: "",
-  //     description: "",
-  //   },
-  //   experience: {
-  //     experience1: {
-  //       jobTitle: "",
-  //       employer: "",
-  //       place: "",
-  //       startDate: "",
-  //       endDate: "",
-  //       description: "",
-  //     },
-  //     experience2: {
-  //       jobTitle: "",
-  //       employer: "",
-  //       place: "",
-  //       startDate: "",
-  //       endDate: "",
-  //       description: "",
-  //     },
-  //     experience3: {
-  //       jobTitle: "",
-  //       employer: "",
-  //       place: "",
-  //       startDate: "",
-  //       endDate: "",
-  //       description: "",
-  //     },
-  //   },
-  //   relevantCourses: "",
-  //   certification: "",
-  //   project: {
-  //     project1: {
-  //       title: "",
-  //       description: "",
-  //       position: "",
-  //     },
-  //     project2: {
-  //       title: "",
-  //       description: "",
-  //       position: "",
-  //     },
-  //   },
-  //   socialLink: {
-  //     link1: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //     link2: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //     link3: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //     link4: {
-  //       label: "",
-  //       link: "",
-  //     },
-  //   },
-  //   skills: "",
-  //   language: "",
-  // });
-
-  // const [userResumeData, setUserResumeData] = useState();
-
   const [userResumeData, setUserResumeData] = useState({
-    fileName: "Harikrishnan-Resume-[23-01-2024]",
+    fileName: "",
     personalDetails: {
-      jobTitle: "Frontend Developer",
-      firstName: "Harikrishnan",
-      lastName: "Prasannakumar",
-      email: "hk17.dev@gmail.com",
-      phone: "7776861386",
-      website: "www.hk17.dev",
+      jobTitle: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      website: "",
     },
-    professionalSummary:
-      "Highly motivated and results-oriented Software Development Engineer (SDE-3) with X years of experience in designing, developing, and deploying complex software solutions. Proven ability to lead and contribute to cross-functional teams, tackle challenging software problems, and deliver high-quality, scalable, and maintainable code. Possesses a strong understanding of system design principles and experience in owning and operating services within a product-based organization.",
+    professionalSummary: "",
     education: {
-      college: "Universal College of Enginereing",
-      degree: "B.Tech in Computer Engineering",
-      place: "Vasai, Kaman",
-      startDate: "2021-07-17",
-      endDate: "2024-07-17",
-      description:
-        "University Name: Universal College of Engineering---Degree: B.Tech in Computer Engineering---Expected Graduation Date: July 2024---GPA (Optional): Include only if above 3.5 or within the honor roll range (e.g., July 2024 (Expected), GPA: 3.8)---Relevant Coursework (Optional): List a few key courses aligned with your target job. (e.g., Data Structures & Algorithms, Software Design Patterns, Computer Networks)---Honors & Awards (Optional): Mention any relevant academic achievements like scholarships, dean's list mentions, or competition awards.---Projects/Activities (Optional): Briefly describe any academic projects or extracurricular activities related to computer science that showcase your skills and interests. (e.g., Senior Capstone Project: Developed a recommendation system using machine learning)---Study Abroad (Optional): If applicable, mention any international study programs you participated in, highlighting the location and duration.",
+      college: "",
+      degree: "",
+      place: "",
+      startDate: "",
+      endDate: "",
+      description: "",
     },
     experience: {
       experience1: {
-        jobTitle: "Software Development Engineer - 2",
-        employer: "Google",
-        place: "Banglore",
-        startDate: "2021-07-17",
-        endDate: "2024-07-17",
-        description:
-          "SDE-2 is also commonly addressed as a Senior Software Engineer or Senior Member of Technical Staff in different organizations.---An SDE-2 by capacity is an individual contributor who works in a team.---When someone is hired as an SDE-2 or gets promoted to an SDE-2 role, it is expected that they have considerable experience and won't need much help in solving straightforward issues.---A Software Development Engineer-2 (or an E4 in the case of Meta) holds end-to-end service ownership in various product-based organizations.",
+        jobTitle: "",
+        employer: "",
+        place: "",
+        startDate: "",
+        endDate: "",
+        description: "",
       },
       experience2: {
-        jobTitle: "Senior FrontEnd Developer",
-        employer: "Google",
-        place: "Banglore",
-        startDate: "2021-07-17",
-        endDate: "2024-07-17",
-        description:
-          "Developing and maintaining complex web applications: This involves writing clean, efficient, and maintainable code using frameworks like React, Angular, or Vue.js, ensuring the application functions smoothly across different browsers and devices.---Collaborating with designers and back-end developers: Working closely with UI/UX designers to understand design goals and integrate them into the development process. Collaborating with back-end developers to ensure seamless integration between the front-end and back-end components.---Optimizing website performance and responsiveness: Implementing best practices for code optimization, performance tuning, and ensuring the website functions well on various devices and internet connection speeds.",
+        jobTitle: "",
+        employer: "",
+        place: "",
+        startDate: "",
+        endDate: "",
+        description: "",
       },
       experience3: {
-        jobTitle: "Research Intern",
-        employer: "OpenAI",
-        place: "Banglore",
-        startDate: "2021-07-17",
-        endDate: "2024-07-17",
-        description:
-          "Machine Learning Expertise: Trained various AI models, demonstrating an understanding of machine learning algorithms and techniques.---Database Management: Handled databases, showcasing proficiency in data storage, manipulation, and retrieval.---Data Analysis and Manipulation: Worked upon data, highlighting your ability to not only store and manage data, but also analyze and extract insights.---Problem-Solving and Adaptability: This overarching point summarizes the potential benefits of combining these skills. By mastering AI models, databases, and data analysis.",
+        jobTitle: "",
+        employer: "",
+        place: "",
+        startDate: "",
+        endDate: "",
+        description: "",
       },
     },
-    relevantCourses:
-      "Frontend Web Development Mastery---SCSS Mastery---Web Design Mastery---Motion Graphics---Web Motions",
-    certification:
-      "Certified Web Professional - Web Developer---Certified Scrum Master (CSM)---Google Data Analytics Professional Certificate---Java Development Certified Professional---Certified Customer Service Specialist (CSS)---Certified Software Development Professional (CSDP)",
+    relevantCourses: "",
+    certification: "",
     project: {
       project1: {
-        title: "RESTful API for a Social Networking Platform",
-        description:
-          "Design endpoints for user management, profile creation, and content sharing.---Implement authentication and authorization mechanisms to ensure data security and privacy.",
-        position: "Project Lead",
+        title: "",
+        description: "",
+        position: "",
       },
       project2: {
-        title: "Content Management System (CMS) Interface:",
-        description:
-          "Managing complex data structures: Content in a CMS can be diverse, requiring you to design user-friendly interfaces for creating, editing, and organizing various types of content, like text, images, and multimedia elements.---Ensuring user experience and efficiency: The interface needs to be intuitive and efficient for users to manage content, including features like drag-and-drop editing, clear navigation, and search functionalities.",
-        position: "Project Lead",
+        title: "",
+        description: "",
+        position: "",
       },
     },
     socialLink: {
       link1: {
-        label: "twitter",
-        link: "twitter.com",
+        label: "",
+        link: "",
       },
       link2: {
-        label: "linkedin",
-        link: "linkedin.com",
+        label: "",
+        link: "",
       },
       link3: {
-        label: "youtube",
-        link: "youtube.com",
+        label: "",
+        link: "",
       },
       link4: {
-        label: "behance",
-        link: "behance.com",
+        label: "",
+        link: "",
       },
     },
-    skills: "ReactJs---NextJs---TailwindCSS---Framer Motion---GSAP---SCSS",
-    language: "Malayalam---English---Hindi---Marathi",
+    skills: "",
+    language: "",
   });
+
+  // const [userResumeData, setUserResumeData] = useState({
+  //   fileName: "Harikrishnan-Resume-[23-01-2024]",
+  //   personalDetails: {
+  //     jobTitle: "Frontend Developer",
+  //     firstName: "Harikrishnan",
+  //     lastName: "Prasannakumar",
+  //     email: "hk17.dev@gmail.com",
+  //     phone: "7776861386",
+  //     website: "www.hk17.dev",
+  //   },
+  //   professionalSummary:
+  //     "Highly motivated and results-oriented Software Development Engineer (SDE-3) with X years of experience in designing, developing, and deploying complex software solutions. Proven ability to lead and contribute to cross-functional teams, tackle challenging software problems, and deliver high-quality, scalable, and maintainable code. Possesses a strong understanding of system design principles and experience in owning and operating services within a product-based organization.",
+  //   education: {
+  //     college: "Universal College of Enginereing",
+  //     degree: "B.Tech in Computer Engineering",
+  //     place: "Vasai, Kaman",
+  //     startDate: "2021-07-17",
+  //     endDate: "2024-07-17",
+  //     description:
+  //       "University Name: Universal College of Engineering---Degree: B.Tech in Computer Engineering---Expected Graduation Date: July 2024---GPA (Optional): Include only if above 3.5 or within the honor roll range (e.g., July 2024 (Expected), GPA: 3.8)---Relevant Coursework (Optional): List a few key courses aligned with your target job. (e.g., Data Structures & Algorithms, Software Design Patterns, Computer Networks)---Honors & Awards (Optional): Mention any relevant academic achievements like scholarships, dean's list mentions, or competition awards.---Projects/Activities (Optional): Briefly describe any academic projects or extracurricular activities related to computer science that showcase your skills and interests. (e.g., Senior Capstone Project: Developed a recommendation system using machine learning)---Study Abroad (Optional): If applicable, mention any international study programs you participated in, highlighting the location and duration.",
+  //   },
+  //   experience: {
+  //     experience1: {
+  //       jobTitle: "Software Development Engineer - 2",
+  //       employer: "Google",
+  //       place: "Banglore",
+  //       startDate: "2021-07-17",
+  //       endDate: "2024-07-17",
+  //       description:
+  //         "SDE-2 is also commonly addressed as a Senior Software Engineer or Senior Member of Technical Staff in different organizations.---An SDE-2 by capacity is an individual contributor who works in a team.---When someone is hired as an SDE-2 or gets promoted to an SDE-2 role, it is expected that they have considerable experience and won't need much help in solving straightforward issues.---A Software Development Engineer-2 (or an E4 in the case of Meta) holds end-to-end service ownership in various product-based organizations.",
+  //     },
+  //     experience2: {
+  //       jobTitle: "Senior FrontEnd Developer",
+  //       employer: "Google",
+  //       place: "Banglore",
+  //       startDate: "2021-07-17",
+  //       endDate: "2024-07-17",
+  //       description:
+  //         "Developing and maintaining complex web applications: This involves writing clean, efficient, and maintainable code using frameworks like React, Angular, or Vue.js, ensuring the application functions smoothly across different browsers and devices.---Collaborating with designers and back-end developers: Working closely with UI/UX designers to understand design goals and integrate them into the development process. Collaborating with back-end developers to ensure seamless integration between the front-end and back-end components.---Optimizing website performance and responsiveness: Implementing best practices for code optimization, performance tuning, and ensuring the website functions well on various devices and internet connection speeds.",
+  //     },
+  //     experience3: {
+  //       jobTitle: "Research Intern",
+  //       employer: "OpenAI",
+  //       place: "Banglore",
+  //       startDate: "2021-07-17",
+  //       endDate: "2024-07-17",
+  //       description:
+  //         "Machine Learning Expertise: Trained various AI models, demonstrating an understanding of machine learning algorithms and techniques.---Database Management: Handled databases, showcasing proficiency in data storage, manipulation, and retrieval.---Data Analysis and Manipulation: Worked upon data, highlighting your ability to not only store and manage data, but also analyze and extract insights.---Problem-Solving and Adaptability: This overarching point summarizes the potential benefits of combining these skills. By mastering AI models, databases, and data analysis.",
+  //     },
+  //   },
+  //   relevantCourses:
+  //     "Frontend Web Development Mastery---SCSS Mastery---Web Design Mastery---Motion Graphics---Web Motions",
+  //   certification:
+  //     "Certified Web Professional - Web Developer---Certified Scrum Master (CSM)---Google Data Analytics Professional Certificate---Java Development Certified Professional---Certified Customer Service Specialist (CSS)---Certified Software Development Professional (CSDP)",
+  //   project: {
+  //     project1: {
+  //       title: "RESTful API for a Social Networking Platform",
+  //       description:
+  //         "Design endpoints for user management, profile creation, and content sharing.---Implement authentication and authorization mechanisms to ensure data security and privacy.",
+  //       position: "Project Lead",
+  //     },
+  //     project2: {
+  //       title: "Content Management System (CMS) Interface:",
+  //       description:
+  //         "Managing complex data structures: Content in a CMS can be diverse, requiring you to design user-friendly interfaces for creating, editing, and organizing various types of content, like text, images, and multimedia elements.---Ensuring user experience and efficiency: The interface needs to be intuitive and efficient for users to manage content, including features like drag-and-drop editing, clear navigation, and search functionalities.",
+  //       position: "Project Lead",
+  //     },
+  //   },
+  //   socialLink: {
+  //     link1: {
+  //       label: "twitter",
+  //       link: "twitter.com",
+  //     },
+  //     link2: {
+  //       label: "linkedin",
+  //       link: "linkedin.com",
+  //     },
+  //     link3: {
+  //       label: "youtube",
+  //       link: "youtube.com",
+  //     },
+  //     link4: {
+  //       label: "behance",
+  //       link: "behance.com",
+  //     },
+  //   },
+  //   skills: "ReactJs---NextJs---TailwindCSS---Framer Motion---GSAP---SCSS",
+  //   language: "Malayalam---English---Hindi---Marathi",
+  // });
+
   const { user } = useContext(userEmailContext);
   const { currentResumeId, setCurrentResumeId } =
     useContext(currentResumeContext);
+
+  const { resumeId } = useParams();
 
   // printing userResumeData ---
   console.clear();
   console.log(user);
   console.log(userResumeData);
+  console.log("RESUME ID --> " + resumeId);
   // ---------------------------
 
   const fetchData = () => {
-    try {
-      const response = axios
-        .get(`${BASEURL}/api/getSingleData?id=${currentResumeId}`)
-        .then((res) => setResumeData(res.data.data[0].data));
-      // console.log(response);
-    } catch (error) {
-      // console.log(error.toJSON());
-      console.log(error);
+    if (resumeId === "newResume") {
+      return;
+    } else {
+      try {
+        const response = axios
+          .get(`${BASEURL}/api/getSingleData?id=${resumeId}`)
+          .then((res) => setUserResumeData(res.data.data[0].data));
+        // console.log(response);
+      } catch (error) {
+        // console.log(error.toJSON());
+        console.log(error);
+      }
     }
   };
 
@@ -222,6 +228,11 @@ const Information = () => {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    fetchData();
+    console.log("alsdjflkajsdflkjalskjdflkajsdflkjalskdfjlksjdflkajklsdjfkl");
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center border-white pb-5">
