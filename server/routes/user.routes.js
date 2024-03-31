3fe481647be41774e8937c32b3414bd4c919adf1
Harikrishnan17 <hyprgd2002@gmail.com>
2024-03-31 18:02:29 +0530

    Added the route for updating resume template route value

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,11 +5,13 @@ const {
   getUserData,
   getUserSingleData,
   updateUserSingleData,
+  updateResumeTemplateRoute,
 } = require("../controllers/userResume.controller.js");
 
 router.post("/addData", addUserData);
 router.get("/getSingleData", getUserSingleData);
 router.get("/getAllData", getUserData);
 router.put("/updateSingleData", updateUserSingleData);
+router.put("/updateResumeTemplateRoute", updateResumeTemplateRoute);
 
 module.exports = router;
