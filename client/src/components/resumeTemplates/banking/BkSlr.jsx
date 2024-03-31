24b295aee473e62fae761e3689b61293151a068c
Harikrishnan17 <hyprgd2002@gmail.com>
2024-03-31 21:10:10 +0530

    Added operator chaining operator in experience 2 section

--- client/src/components/resumeTemplates/banking/BkSlr.jsx
+++ client/src/components/resumeTemplates/banking/BkSlr.jsx
@@ -159,32 +159,32 @@ const BkSlr = () => {
                     {/* header section */}
                     <h1 className="flex flex-col-reverse justify-start font-roboto text-[18px]">
                       <span className="text-base font-normal text-[#58595b]">
-                        {resumeData.experience.experience2.jobTitle}
+                        {resumeData?.experience?.experience2?.jobTitle}
                       </span>
                     </h1>
                     {/* Date and place section */}
                     <div className="flex items-center justify-between text-sm italic text-gray-500">
                       <p className="">
                         <span className="">
-                          {resumeData.experience.experience2.employer}
+                          {resumeData?.experience?.experience2?.employer}
                         </span>
                         {", "}
                         <span className="">
-                          {resumeData.experience.experience2.place}
+                          {resumeData?.experience?.experience2?.place}
                         </span>
                         {" | "}
                         <span className="">
-                          {resumeData.experience.experience2.startDate}
+                          {resumeData?.experience?.experience2?.startDate}
                         </span>
                         {" - "}
                         <span className="">
-                          {resumeData.experience.experience2.endDate}
+                          {resumeData?.experience?.experience2?.endDate}
                         </span>
                       </p>
                     </div>
                     {/* description section - 1 */}
                     <ul className="ml-10 list-disc">
-                      {resumeData.experience.experience2.description
+                      {resumeData?.experience?.experience2?.description
                         .split("---")
                         .map((item, i) => (
                           <li key={i} className="text-xs">
