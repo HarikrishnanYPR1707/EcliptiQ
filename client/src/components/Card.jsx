56bebc4223f1e28e7d6536d0a84306b6f12f9918
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-02 10:25:20 +0530

    Added some changes

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Card = () => {
-  return <div>card</div>;
+const Card = ({ fileName, resumeRoute }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => navigate("/alsdkjf")}
+      className="w-fit cursor-pointer rounded-lg border border-purple-500 px-5 py-2 font-bold"
+    >
+      Harikrishnan-resume-[01-01-1001]
+    </div>
+  );
 };
 
 export default Card;
