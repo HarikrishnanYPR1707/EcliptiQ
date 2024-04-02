0f160b9407a2a05415487160b6fd1574d063f99f
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-02 09:49:04 +0530

    Added user profile image url context

--- client/src/Contexts/userDataContext.js
+++ client/src/Contexts/userDataContext.js
@@ -1,4 +1,5 @@
 import { createContext } from "react";
 
+export const userProfileUrlContext = createContext();
 export const userEmailContext = createContext();
 export const isUserLoginContext = createContext();
