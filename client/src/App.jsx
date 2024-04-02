5f7a9c9376f7713931273f61f5389bb23a67c814
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-02 09:53:41 +0530

    Added profile url context provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import Template from "./pages/Template";
 import Information from "./pages/Information";
 import KeyWords from "./pages/KeyWords";
 import {
+  userProfileUrlContext,
   userEmailContext,
   isUserLoginContext,
 } from "./Contexts/userDataContext";
@@ -25,41 +26,49 @@ const App = () => {
   const [user, setUser] = useState("");
   const [isLogin, setIsLogin] = useState(false);
   const [currentResumeId, setCurrentResumeId] = useState();
+  const [userProfileUrlLink, setUserProfileUrlLink] = useState();
   return (
     <>
-      <currentResumeContext.Provider
-        value={{ currentResumeId, setCurrentResumeId }}
+      <userProfileUrlContext.Provider
+        value={{ userProfileUrlLink, setUserProfileUrlLink }}
       >
-        <isUserLoginContext.Provider value={{ isLogin, setIsLogin }}>
-          <userEmailContext.Provider value={{ user, setUser }}>
-            <Router>
-              <Navbar />
-              <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/templates" element={<Template />} />
-                  <Route
-                    path="/information/:resumeId"
-                    element={<Information />}
-                  />
-                  <Route path="/keyword" element={<KeyWords />} />
-                  <Route path="/profile/:userName" element={<UserProfile />} />
-                  <Route path="/templates/sde/elr" element={<SdeElr />} />
-                  <Route path="/templates/sde/jlr" element={<SdeJlr />} />
-                  <Route path="/templates/sde/slr" element={<SdeSlr />} />
-                  <Route path="/templates/da/elr" element={<DaElr />} />
-                  <Route path="/templates/da/jlr" element={<DaJlr />} />
-                  <Route path="/templates/da/slr" element={<DaSlr />} />
-                  <Route path="/templates/bk/elr" element={<BkElr />} />
-                  <Route path="/templates/bk/jlr" element={<BkJlr />} />
-                  <Route path="/templates/bk/slr" element={<BkSlr />} />
-                  <Route path="*" element={<div>404 Page</div>} />
-                </Routes>
-              </div>
-            </Router>
-          </userEmailContext.Provider>
-        </isUserLoginContext.Provider>
-      </currentResumeContext.Provider>
+        <currentResumeContext.Provider
+          value={{ currentResumeId, setCurrentResumeId }}
+        >
+          <isUserLoginContext.Provider value={{ isLogin, setIsLogin }}>
+            <userEmailContext.Provider value={{ user, setUser }}>
+              <Router>
+                <Navbar />
+                <div className="mx-auto w-full max-w-[1550px] stopper:px-0">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/templates" element={<Template />} />
+                    <Route
+                      path="/information/:resumeId"
+                      element={<Information />}
+                    />
+                    <Route path="/keyword" element={<KeyWords />} />
+                    <Route
+                      path="/profile/:userName"
+                      element={<UserProfile />}
+                    />
+                    <Route path="/templates/sde/elr" element={<SdeElr />} />
+                    <Route path="/templates/sde/jlr" element={<SdeJlr />} />
+                    <Route path="/templates/sde/slr" element={<SdeSlr />} />
+                    <Route path="/templates/da/elr" element={<DaElr />} />
+                    <Route path="/templates/da/jlr" element={<DaJlr />} />
+                    <Route path="/templates/da/slr" element={<DaSlr />} />
+                    <Route path="/templates/bk/elr" element={<BkElr />} />
+                    <Route path="/templates/bk/jlr" element={<BkJlr />} />
+                    <Route path="/templates/bk/slr" element={<BkSlr />} />
+                    <Route path="*" element={<div>404 Page</div>} />
+                  </Routes>
+                </div>
+              </Router>
+            </userEmailContext.Provider>
+          </isUserLoginContext.Provider>
+        </currentResumeContext.Provider>
+      </userProfileUrlContext.Provider>
     </>
   );
 };
