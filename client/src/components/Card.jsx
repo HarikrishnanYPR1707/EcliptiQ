54105d1ee3e70ded56503469fcf88e4e465c771d
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-02 10:50:14 +0530

    added some content

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ fileName, resumeRoute }) => {
   return (
     <div
       onClick={() => navigate("/alsdkjf")}
-      className="w-fit cursor-pointer rounded-lg border border-purple-500 px-5 py-2 font-bold"
+      className="w-[310px] cursor-pointer rounded-lg border border-purple-500 px-5 py-2 text-center font-bold"
     >
       Harikrishnan-resume-[01-01-1001]
     </div>
