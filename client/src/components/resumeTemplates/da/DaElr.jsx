886fa3125780745d7d42320cf5fd7898ab4ef800
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-03 20:07:34 +0530

    Added update button to da elr

--- client/src/components/resumeTemplates/da/DaElr.jsx
+++ client/src/components/resumeTemplates/da/DaElr.jsx
@@ -8,6 +8,7 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaGraduationCap } from "react-icons/fa";
 import Cookies from "js-cookie";
 import { updateResumeTemplateRoute, fetchData } from "@/utils/DataFunctions";
+import { Link } from "react-router-dom";
 
 const DaElr = () => {
   const daElrComponentDownloadRef = useRef(null);
@@ -309,12 +310,20 @@ const DaElr = () => {
             </div>
           </div>
         </div>
-        <button
-          onClick={handlePrint}
-          className="rounded-lg bg-purple-500 px-3 py-2 text-sm font-bold"
-        >
-          Download Template
-        </button>
+        <div className="mb-5 flex w-[800px] items-center justify-around">
+          <button
+            onClick={handlePrint}
+            className="w-[200px] rounded-lg bg-purple-500 px-3 py-2 text-sm font-bold"
+          >
+            Download Template
+          </button>
+          <Link
+            to={`/information/${currentResumeId}`}
+            className="w-[200px] rounded-lg bg-purple-500 px-3 py-2 text-center text-sm font-bold"
+          >
+            Update
+          </Link>
+        </div>
       </div>
     </div>
   );
